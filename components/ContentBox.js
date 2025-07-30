5b92d5b22a12e69fd3eb47c15cf3bd758445204d
Goutam Roy <167274642+Goutam245@users.noreply.github.com>
2025-07-30 14:53:52 +0600

    Update README.md

--- components/ContentBox.js
+++ components/ContentBox.js
@@ -1,4 +1,7 @@
 import styles from "./ContentBox.module.css";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const ContentBox = ({ children }) => {
   return <div className={styles.wrapper}>{children}</div>;
